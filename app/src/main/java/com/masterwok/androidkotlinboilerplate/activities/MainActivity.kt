9b2b3d0e8bb37fd1cf860c975895d0bcdbf4dc52
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-07-15 13:39:10 -0400

    master - simple livedata example.

--- app/src/main/java/com/masterwok/androidkotlinboilerplate/activities/MainActivity.kt
+++ app/src/main/java/com/masterwok/androidkotlinboilerplate/activities/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.masterwok.androidkotlinboilerplate.activities
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
+import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.AppCompatTextView
 import com.masterwok.androidkotlinboilerplate.R
 import com.masterwok.androidkotlinboilerplate.viewmodels.MainActivityViewModel
 
 class MainActivity : BaseActivity<MainActivityViewModel>() {
 
+    private var buttonClickMe: AppCompatButton? = null
+    private var textViewCount: AppCompatTextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        bindViewComponents()
+        subscribeToViewComponents()
+        subscribeToLiveData()
+    }
+
+    private fun bindViewComponents() {
+        buttonClickMe = findViewById(R.id.button_click_me)
+        textViewCount = findViewById(R.id.textview_count)
+    }
+
+    private fun subscribeToViewComponents() {
+        buttonClickMe?.setOnClickListener {
+            val currentCount = viewModel.getCurrentCount()
+
+            currentCount.value = currentCount.value?.plus(1)
+        }
+    }
+
+    private fun subscribeToLiveData() {
+        viewModel
+                .getCurrentCount()
+                .observe(this, Observer { t -> configure(t) })
+    }
+
+    private fun configure(count: Int?) {
+        textViewCount?.text = count?.toString()
     }
 
 }
--- app/src/main/java/com/masterwok/androidkotlinboilerplate/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/masterwok/androidkotlinboilerplate/viewmodels/MainActivityViewModel.kt
@@ -1,6 +1,20 @@
 package com.masterwok.androidkotlinboilerplate.viewmodels
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import javax.inject.Inject
 
-class MainActivityViewModel @Inject constructor() : ViewModel()
+class MainActivityViewModel @Inject constructor() : ViewModel() {
+
+    private var currentCount: MutableLiveData<Int> = MutableLiveData()
+
+    fun getCurrentCount(): MutableLiveData<Int> {
+        if (currentCount.value == null) {
+            currentCount.value = 0
+        }
+
+        return currentCount
+    }
+
+
+}
